6b372783cdaa723d6bd36c45d525e3988030394e
Wither <Wither@Wither-PC>
2015-11-07 03:03:26 +0800

    T

---
+++ SMXAlgorithmVerification/src/main/java/com/yinwenqi/ui/VerifyUI.java
@@ -0,0 +1,48 @@
+package com.yinwenqi.ui;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.yinwenqi.model.User;
+
+@Controller
+public class VerifyUI {
+
+	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+	public String show(@PathVariable int id, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		System.out.println("id------------------------>" + id);
+		return "user";
+	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public String test(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		//RequestDispatcher rd = request.getRequestDispatcher("/welcome.html");  
+        //rd.forward(request, response);  
+		//response.setStatus(200);
+        //response.sendRedirect("/welcome.html");  
+		return "test";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	@ResponseBody
+	public String create(@RequestBody(required = true) User user, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		return "success";
+	}
+	
+	@RequestMapping(value="index.do")  
+    public void index_jsp(Model model){  
+        model.addAttribute("liming", "黎明你好");  
+        System.out.println("index.jsp");  
+    }  
+}
---
+++ SMXAlgorithmVerification/src/main/webapps/WEB-INF/views/test.jsp
@@ -0,0 +1,28 @@
+﻿<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+    
+<html dir="ltr" lang="zh_CN">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+	<title>JavaScript版国密算法的实现</title>
+
+	<!--- CSS --->
+	<link rel="stylesheet" href="style.css" type="text/css" />
+
+	</head>
+
+	<body>
+		<div id="container">
+			<div class="welcome">
+				<div class="welcome-user">欢迎回来，yinwenqi</div>
+				<div class="welcome-text">这是一个WEB程序，用来验证JavaScript版国密算法</div>
+				<div class="home"><a href="index.html">退出</a></div>
+			</div>
+		</div>
+		<div id="footer">
+			JavaScript版国密算法的实现   <a href="http://www.mycodes.net/" target="_blank" title="关于">关于</a>
+		</div>
+	</body>
+</html>
